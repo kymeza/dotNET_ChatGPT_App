7c44ddb061ecb68d3b559bab6f7d6f324c418cb7
Kyron Meza <kyron.meza@vivint.com>
2024-09-24 21:57:06 -0300

    Frontend Commited

---
+++ Backend/Backend/Hubs/ChatStreamingHub.cs
@@ -0,0 +1,75 @@
+﻿using System.ClientModel;
+using System.Collections.Concurrent;
+using System.Threading.Channels;
+using Backend.Models.Config;
+using Microsoft.AspNetCore.SignalR;
+using OpenAI.Chat;
+
+namespace Backend.Hubs;
+
+public class ChatStreamingHub : Hub
+{
+    private static ConcurrentDictionary<string, ChatClient> _clients = new();
+
+    private OpenAiSettings _openAiSettings;
+
+    public ChatStreamingHub(OpenAiSettings openAiSettings)
+    {
+        _openAiSettings = openAiSettings;
+    }
+
+    // Called when a client connects
+    public override async Task OnConnectedAsync()
+    {
+        string clientId = Context.ConnectionId;
+        Console.WriteLine($"Client {clientId} connected.");
+    }
+    
+    public async Task CreateClient(string model = "gpt-3.5-turbo")
+    {
+        string clientId = Context.ConnectionId;
+        var gptInstance = new ChatClient(apiKey: _openAiSettings.ApiKey, model: model);
+        
+        // Store the instance for this connection
+        _clients.TryAdd(clientId, gptInstance);
+
+        Console.WriteLine($"Client {clientId} created a ChatGPT instance.");
+    }
+    
+    public async IAsyncEnumerable<string> SendCompletion(string message)
+    {
+        string clientId = Context.ConnectionId;
+
+        if (_clients.TryGetValue(clientId, out var gptInstance))
+        {
+            AsyncCollectionResult<StreamingChatCompletionUpdate> updates = gptInstance.CompleteChatStreamingAsync(message);
+            await foreach (StreamingChatCompletionUpdate update in updates)
+            {
+                foreach (ChatMessageContentPart updatePart in update.ContentUpdate)
+                {
+                    yield return updatePart.Text;
+                }
+            }
+        }
+    }
+    
+    // Called when a client disconnects
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        string clientId = Context.ConnectionId;
+
+        // Remove the instance for this connection
+        if (_clients.TryRemove(clientId, out var gptInstance))
+        {
+            // Dispose of the client if necessary
+            if (gptInstance is IDisposable disposableClient)
+            {
+                disposableClient.Dispose();
+            }
+        }
+
+        Console.WriteLine($"Client {clientId} disconnected.");
+
+        await base.OnDisconnectedAsync(exception);
+    }
+}
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,11 +1,36 @@
+using Backend;
+using Backend.Hubs;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("https://localhost:7288") // Specify the exact origin
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()); // Allow credentials is important for SignalR
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+// Add configuration or service dependencies here.
+ServicesConfiguration.RegisterServices(builder.Services, builder.Configuration);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -18,8 +43,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting();
+
+app.MapHub<ChatStreamingHub>("/chatStreamingHub");
+
+app.UseCors("AllowSpecificOrigin");
+
 
 app.Run();
