eabeb7b40eee34f426dbb17caf20a55916e912be
Kyron Meza <kyron.meza@xerevro.com>
2024-10-18 10:59:03 -0300

    Added GetUser endpoint

--- Backend/Backend/Controller/UserController.cs
+++ Backend/Backend/Controller/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Backend.Domain.Helpers;
 using Backend.Domain.Repositories.AppDbContext;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controller;
 
@@ -21,6 +22,20 @@ public class UserController : ControllerBase
         user.Password = Argon2PasswordHasher.HashPassword(user.Password);
         _appDbContext.Users.Add(user);
         _appDbContext.SaveChanges();
+        
+        return Ok(user);
+    }
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> Get([FromRoute] string username)
+    {
+        var user = await _appDbContext.Users
+            .FirstOrDefaultAsync(u => u.Username == username);
+
+        if (user == null)
+        {
+            return NotFound($"User with username '{username}' not found.");
+        }
         
         return Ok(user);
     }
