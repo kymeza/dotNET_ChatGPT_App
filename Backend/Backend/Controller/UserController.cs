4f9cf90ff1f06ba095f9e012ce14c87561685e36
Kyron Meza <kyron.meza@xerevro.com>
2024-10-22 09:31:15 -0300

    minor changes

--- Backend/Backend/Controller/UserController.cs
+++ Backend/Backend/Controller/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Backend.Domain.Helpers;
 using Backend.Domain.Repositories.AppDbContext;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,7 @@ namespace Backend.Controller;
 
 [ApiController]
 [Route("api/users")]
+[Authorize]
 public class UserController : ControllerBase
 {
     private readonly AppDbContext _appDbContext;
