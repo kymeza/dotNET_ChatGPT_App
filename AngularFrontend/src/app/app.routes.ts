487c91b3e14cee04847f15920b8c646404f993ce
Kyron Meza <kyron.meza@xerevro.com>
2024-10-25 10:27:40 -0300

    upload file component created

--- AngularFrontend/src/app/app.routes.ts
+++ AngularFrontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../auth/login.component';
 import { ChatComponent } from '../chat/chat.component';
 import { AuthGuard } from '../auth/auth.guard';
+import { UploadFileComponent } from '../fileUpload/fileupload.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'chat', component: ChatComponent, canActivate: [AuthGuard]},
+    {path: 'upload', component: UploadFileComponent, canActivate: [AuthGuard]},
     {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
