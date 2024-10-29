f98c14ab59aa39a5bd2feb4884a89d14f8242243
Kyron Meza <kyron.meza@xerevro.com>
2024-10-29 09:09:58 -0300

    Updload File endpoint created

--- Backend/Backend/Controller/VulnerableController.cs
+++ Backend/Backend/Controller/VulnerableController.cs
@@ -2,12 +2,14 @@
 using AutoMapper;
 using Backend.Models.Dtos;
 using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controller;
 
 [ApiController]
 [Route("api/vulnerable")]
+[Authorize]
 public class VulnerableController : ControllerBase
 {
     private readonly ILogger<VulnerableController> _logger;
@@ -54,6 +56,41 @@ public class VulnerableController : ControllerBase
         
         var product = await _dbConnection.QueryAsync<ProductDto>(query, new { idArticulo = id });
         return Ok(product);
-    } 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadFile([FromForm] IFormFile file, [FromForm] string fileName)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file provided.");
+        }
+        
+        var uploadPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads");
+        
+        var extension = Path.GetExtension(file.FileName);
+        
+        fileName = fileName + extension;
+        
+        var fullPath = Path.Combine(uploadPath, fileName);
+        
+        try
+        {
+            // Save file to disk
+            using (var stream = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
+            {
+                await file.CopyToAsync(stream);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions, such as access denied, path not found, etc.
+            return StatusCode(500, "Internal Server Error: " + ex.Message);
+        }
+
+        return Ok("File uploaded successfully.");
+        
+    }
+    
     
 }
