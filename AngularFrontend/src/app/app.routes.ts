16e12e8d43ec3abe2e65a114af58c45b03fbbefe
Kyron Meza <kyron.meza@xerevro.com>
2024-10-29 10:53:34 -0300

    command view created

--- AngularFrontend/src/app/app.routes.ts
+++ AngularFrontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../auth/login.component'; // Your standalone components
 import { UploadFileComponent } from '../fileUpload/fileupload.component';
 import { ChatComponent } from '../chat/chat.component';
+import { CommandComponent } from '../command/command.component';
 import { AuthGuard } from '../auth/auth.guard'; // Your auth guard
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'chat', component: ChatComponent, canActivate: [AuthGuard] },
   {path: 'upload', component: UploadFileComponent, canActivate: [AuthGuard]},
+  {path: 'command', component: CommandComponent, canActivate: [AuthGuard]},
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
