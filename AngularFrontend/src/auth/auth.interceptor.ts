301de6825a8411aee24d9e920823427d44de6c2b
Kyron Meza <kyron.meza@xerevro.com>
2024-11-04 10:02:54 -0300

    Supertienda Products view created

--- AngularFrontend/src/auth/auth.interceptor.ts
+++ AngularFrontend/src/auth/auth.interceptor.ts
@@ -1,20 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.getToken();
-    if (token) {
-      const cloned = req.clone({
+  const clonedReq = token
+    ? req.clone({
         headers: req.headers.set('Authorization', `Bearer ${token}`)
-      });
-      return next.handle(cloned);
-    }
-    return next.handle(req);
-  }
-}
+      })
+    : req;
+
+  return next(clonedReq);
+};
